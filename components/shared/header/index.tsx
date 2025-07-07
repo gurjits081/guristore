6ec4e1c80319d4306e4e02b8cd82619fa8279e2f
Gurjit Singh <gurjits081@gmail.com>
2025-07-07 19:21:41 +0530

    added a drawer component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,10 +1,7 @@
-import {ShoppingCart, UserIcon} from 'lucide-react'
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from '@/lib/constants';
-import ModeToggle from './mode-toggle';
-
+import Menu from './menu';
 
 const Header = () => {
     return ( 
@@ -18,19 +15,7 @@ const Header = () => {
                         {APP_NAME}
                     </span>
                 </div>
-                <div className="space-x-2">
-                    <ModeToggle />
-                    <Button asChild variant='ghost'>
-                        <Link href="/cart">
-                         <ShoppingCart /> Cart
-                        </Link>
-                    </Button>
-                     <Button asChild>
-                        <Link href="/signin">
-                         <UserIcon /> Sign In
-                        </Link>
-                    </Button>
-                </div>
+                <Menu />
             </div>
         </header>
      );
