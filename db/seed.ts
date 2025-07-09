490e77815b7acdabc32e3ab4695f100bfdffb7b0
Gurjit Singh <gurjits081@gmail.com>
2025-07-09 21:20:54 +0530

    database seeding

---
+++ db/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@/lib/generated/prisma';
+import sampleData from './sample-data';
+
+const prisma = new PrismaClient();
+async function main() {
+    await prisma.product.deleteMany();
+
+    await prisma.product.createMany({ data: sampleData.products });
+
+    console.log('Database seeded successfully')
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
