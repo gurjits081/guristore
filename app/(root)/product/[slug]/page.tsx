e28e74a5078d243a976b8b320ee9caf7d8594a1b
Gurjit Singh <gurjits081@gmail.com>
2025-07-10 21:24:43 +0530

    added product images component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
     params: Promise<{ slug:string }>
@@ -18,7 +19,7 @@ const ProductDetailsPage = async (props: {
             <div className="grid gric-cols-1 md:grid-cols-5">
                {/* Image Col */}
                 <div className="col-span-2">
-                {/* Image component */}
+                <ProductImages images={product.images}/>
                 </div>
                 <div className="col-span-2 p-5">
                     <div className="flex flex-col gap-6">
